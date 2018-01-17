12463240f590c654b1e569d5a77fcb0ecd874d1a
Roey <roeyb1@hotmail.com>
2018-01-17 10:22:27 -0500

    Fixed link to image viewer

--- js/galleryScript.js
+++ js/galleryScript.js
@@ -4,16 +4,16 @@ var imageViewer=document.getElementById("imageViewer");
 
 function expandImage(id){
 	var selectedImage=document.getElementById(id);
-	
+
 	var widthOfImage=selectedImage.width;
 	var heightOfImage=selectedImage.height;
-	
-	featuredImage.src="images/"+id+".jpg";
+
+	featuredImage.src="../media/gallery/"+id+".jpg";
 	imageViewer.style.visibility="visible";
-	
+
 	featuredImage.width=widthOfImage*1.2;
 	featuredImage.height=heightOfImage*1.2;
-	
+
 /*	imageViewer.style.width=featuredImage.width+"px";
 	imageViewer.style.height=featuredImage.height+"px";*/
 }
@@ -28,7 +28,7 @@ var galleryContainers;
 function galleryResize(){
 	var windowWidth=window.innerWidth;
 	galleryContainers=document.getElementsByClassName("galleryImageContainer");
-	
+
 	if (windowWidth<500){
 			for (var i=0; i<galleryContainers.length;i++){
 				var container=galleryContainers[i];
