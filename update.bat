128b6ca16312e9cf72f6b260468564dcd0f8795b
merny93 <simtart@gmail.com>
2018-01-18 15:54:01 -0500

    tried to make a penis

--- update.bat
+++ update.bat
@@ -1,5 +1,8 @@
 cls
 
+ECHO We are about to update the server wowza
+pause
+
 del /q X:\css
 del /q X:\en
 del /q X:\fr
@@ -9,4 +12,6 @@ COPY /y C:\Users\Simon\Documents\GitHub\roboticswebsite\css X:\css
 COPY /y C:\Users\Simon\Documents\GitHub\roboticswebsite\js X:\js
 COPY /y C:\Users\Simon\Documents\GitHub\roboticswebsite\en X:\en
 COPY /y C:\Users\Simon\Documents\GitHub\roboticswebsite\fr X:\fr
- pause
+
+ECHO all done
+pause
