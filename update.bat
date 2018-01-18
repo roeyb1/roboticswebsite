540d4698862a6a97dc8bc4165ea0e9760ca7344c
merny93 <simtart@gmail.com>
2018-01-18 15:42:11 -0500

    pdate file

---
+++ update.bat
@@ -0,0 +1,12 @@
+cls
+
+del /q X:\css
+del /q X:\en
+del /q X:\fr
+del /q X:\js
+
+COPY /y C:\Users\Simon\Documents\GitHub\roboticswebsite\css X:\css
+COPY /y C:\Users\Simon\Documents\GitHub\roboticswebsite\js X:\js
+COPY /y C:\Users\Simon\Documents\GitHub\roboticswebsite\en X:\en
+COPY /y C:\Users\Simon\Documents\GitHub\roboticswebsite\fr X:\fr
+ pause
